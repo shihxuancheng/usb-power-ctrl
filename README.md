98701cba0042cf21fd47ad757d90401597cc8e34
richard <shihxuancheng@gmail.com>
2021-10-07 00:18:52 +0800

    revision-1

---
+++ README.md
@@ -0,0 +1,42 @@
+# usb-power-ctrl
+
+## Run
+
+### A. control the usb power per-port on raspberry pi
+
+1. list the status of all supported usb hub
+
+```shell
+$ uhubctl
+```
+
+2. cut the power of port2 of the usb hub 2
+
+```shell
+$ uhubctl -l 2 -a off -p 2
+```
+
+### B. run the python script in background
+
+1. using the `nuhup` command to start
+
+```shell
+(venv)$ nuhup python -u main.py
+```
+
+2. find the process id
+
+```shell
+(venv)$ ps ax | grep main.py
+```
+
+3. terminate the background process
+
+```shell
+(venv)$ kill $PID
+```
+
+## Reference
+
+- https://github.com/mvp/uhubctl
+- https://www.twblogs.net/a/5cb6cb69bd9eee0f00a1e039
--- src/main.py
+++ src/main.py
@@ -1,29 +1,44 @@
-from typing import Union
+import atexit
 import os
+from typing import Union
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 
-def schedule_method():
-    print('on Scheduler triggered!!!')
+# power on all ports when program exit
+@atexit.register
+def reset_power_status():
+    os.system('sudo uhubctl -l 2 -a 1')
 
 
 def switch_power(hub_id: int, port: str, on_off: Union[int, str]):
-    if len(port) == 0:
-        # 不指定usb port
-        print('switch all ports  to ' + on_off)
-        # os.system('dir')
-    else:
-        print('switch port ' + port + ' to ' + on_off)
+    try:
+        if len(port) == 0:
+            # 不指定usb port
+            # print('switch all ports  to ' + on_off)
+            command = 'sudo uhubctl -l ' + str(hub_id) + ' -a ' + str(on_off)
+            print('Command: ' + command)
+            os.system(command)
+        else:
+            command = ' sudo uhubctl -l ' + str(hub_id) + ' -p ' + str(port) + ' -a ' + str(on_off)
+            print('Command: ' + command)
+            os.system(command)
+            # print('switch port ' + port + ' to ' + on_off)
+    except Exception as e:
+        print(str(e))
 
 
 if __name__ == '__main__':
+    switch_power(2, '', 0)  # turn off the power of all usb ports
+
     scheduler = BlockingScheduler()
 
-    # job to switch power on
-    scheduler.add_job(switch_power, CronTrigger(hour='15', minute='46'), args=[2, '', '0'])
+    # scheduler.add_job(switch_power, CronTrigger(second='*/10'), args=[2, '', '1'])
 
-    # job to switch power off
-    scheduler.add_job(switch_power, CronTrigger(hour='15', minute='36'), args=[2, '', '1'])
+    # job to switch power on at 20:00 everyday
+    scheduler.add_job(switch_power, CronTrigger(hour='20'), args=[2, '', '1'])
+
+    # job to switch power off at 07:00 everyday
+    scheduler.add_job(switch_power, CronTrigger(hour='7'), args=[2, '', '0'])
 
     scheduler.start()
